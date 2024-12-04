e68837b0094e0969d00289443e91a1cded99c733
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2024-12-03 22:31:17 -0600

    Subimos la alerta de verificaciones

--- resources/views/catalogos/verificaciones/index.blade.php
+++ resources/views/catalogos/verificaciones/index.blade.php
@@ -1,42 +1,43 @@
 @extends('layouts.app')
 
 @section('body')
-    
     <div class="px-4 py-6">
         <!-- Mapa de sitio -->
         <div class="flex justify-end mt-2 mb-4">
-                <nav class="text-sm text-gray-600">
-                    <div class="flex items-center space-x-4">
-                        <li class="flex items-center">
-                            <a href="{{ route('Gestion') }}" title="Ir a la gestión de vehículos" class="flex items-center text-gray-700 hover:text-gray-900">
-                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
-                                </svg>
-                                Gestión
-                            </a>
-                        </li>
-                        <!-- Separador -->
-                        <p class="text-gray-500">/</p>
-                        <!-- Catálogos -->
-                        <li class="flex items-center">
-                            <a href="{{ $backRoute ?? route('catalogos.index') }}" title="Volver a Catálogos" class="text-gray-800 hover:text-gray-800">
-                                Catálogos
-                            </a>
-                        </li>
-                        <!-- Separador -->
-                        <p class="text-gray-500">/</p>
-                        <!-- Seguros -->
-                        <li class="flex items-center">
-                            <p  class="text-gray-800 hover:text-gray-800">
-                                Verificaciones
-                            </p>
-                        </li>
+            <nav class="text-sm text-gray-600">
+                <div class="flex items-center space-x-4">
+                    <li class="flex items-center">
+                        <a href="{{ route('Gestion') }}" title="Ir a la gestión de vehículos"
+                            class="flex items-center text-gray-700 hover:text-gray-900">
+                            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
+                            </svg>
+                            Gestión
+                        </a>
+                    </li>
+                    <!-- Separador -->
+                    <p class="text-gray-500">/</p>
+                    <!-- Catálogos -->
+                    <li class="flex items-center">
+                        <a href="{{ $backRoute ?? route('catalogos.index') }}" title="Volver a Catálogos"
+                            class="text-gray-800 hover:text-gray-800">
+                            Catálogos
+                        </a>
+                    </li>
+                    <!-- Separador -->
+                    <p class="text-gray-500">/</p>
+                    <!-- Seguros -->
+                    <li class="flex items-center">
+                        <p class="text-gray-800 hover:text-gray-800">
+                            Verificaciones
+                        </p>
+                    </li>
                     </ul>
-                </nav>
+            </nav>
         </div>
         <div class="p-6 bg-white rounded-md shadow-md">
 
@@ -54,7 +55,8 @@
                                 class="w-full px-4 py-2 text-gray-700 border rounded-l-md focus:outline-none md:w-48"
                                 value="{{ request('search') }}" title="Buscar Verificación">
                             <button type="submit"
-                                class="flex items-center px-4 py-2 ml-1 text-white bg-blue-600 border-l-0 rounded-r-md hover:bg-blue-700 focus:outline-none" title="Realizar búsqueda">
+                                class="flex items-center px-4 py-2 ml-1 text-white bg-blue-600 border-l-0 rounded-r-md hover:bg-blue-700 focus:outline-none"
+                                title="Realizar búsqueda">
                                 Buscar
                             </button>
                         </div>
@@ -65,10 +67,12 @@
                 <div class="flex justify-end ml-2 space-x-2">
                     {{-- Registro --}}
                     <a href="{{ route('verificaciones.create') }}"
-                        class="inline-block px-4 py-3 text-white bg-blue-600 rounded hover:bg-blue-700" title="Nuevo registro de verificación">Nuevo registro</a>
+                        class="inline-block px-4 py-3 text-white bg-blue-600 rounded hover:bg-blue-700"
+                        title="Nuevo registro de verificación">Nuevo registro</a>
                     {{-- Calendario --}}
                     <button id="btn-calendar"
-                        class="inline-flex items-center px-6 py-3 text-base font-medium text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" title="Ver calendario de verificaciones">
+                        class="inline-flex items-center px-6 py-3 text-base font-medium text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        title="Ver calendario de verificaciones">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24"
                             fill="none" class="svg-icon">
                             <g stroke-width="2" stroke-linecap="round" stroke="#fff">
@@ -101,19 +105,19 @@
                                     {{ $vr->automovil->marca }}-{{ $vr->automovil->submarca }}-{{ $vr->automovil->modelo }}
                                 </td>
                                 <td class="px-4 py-2 border">
-                                     {{ \Carbon\Carbon::parse( $vr->fecha_verificacion )->locale('es')->format('d-m-Y') }}
+                                    {{ \Carbon\Carbon::parse($vr->fecha_verificacion)->locale('es')->format('d-m-Y') }}
 
-                                    </td>
+                                </td>
                                 <td class="px-4 py-2 border">
-                                     {{ \Carbon\Carbon::parse($vr->proxima_verificacion )->locale('es')->format('d-m-Y') }}
+                                    {{ \Carbon\Carbon::parse($vr->proxima_verificacion)->locale('es')->format('d-m-Y') }}
 
-                                    </td>
+                                </td>
                                 {{-- acciones --}}
                                 <td class="px-4 py-2 border">
                                     <div class="flex items-center space-x-2">
                                         <!-- Ver -->
                                         <a href="{{ route('verificaciones.show', $vr->id_verificacion) }}"
-                                            class="inline-flex items-center justify-center w-8 h-8 text-green-600 border border-green-600 rounded hover:bg-green-600 hover:text-white" 
+                                            class="inline-flex items-center justify-center w-8 h-8 text-green-600 border border-green-600 rounded hover:bg-green-600 hover:text-white"
                                             title="Ver detalles de la verificación">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke="currentColor" class="w-4 h-4">
@@ -124,7 +128,7 @@
 
                                         <!-- Editar -->
                                         <a href="{{ route('verificaciones.edit', $vr->id_verificacion) }}"
-                                            class="inline-flex items-center justify-center w-8 h-8 text-yellow-600 border border-yellow-600 rounded hover:bg-yellow-600 hover:text-white" 
+                                            class="inline-flex items-center justify-center w-8 h-8 text-yellow-600 border border-yellow-600 rounded hover:bg-yellow-600 hover:text-white"
                                             title="Editar la verificación">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke="currentColor" class="w-4 h-4">
@@ -139,10 +143,10 @@
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" onclick="deleteRegister(event)"
-                                                class="inline-flex items-center justify-center w-8 h-8 text-red-600 border border-red-600 rounded hover:bg-red-600 hover:text-white" 
+                                                class="inline-flex items-center justify-center w-8 h-8 text-red-600 border border-red-600 rounded hover:bg-red-600 hover:text-white"
                                                 title="Borrar la verificación">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                    stroke="currentColor" class="w-4 h-4">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                         d="M19 7H5M10 11v6m4-6v6M7 7h10l-1-1H8l-1 1z" />
                                                 </svg>
@@ -211,5 +215,18 @@
                 }
             });
         }
+    </script>
+    {{-- calendar  --}}
+    <script>
+        const btnAlert = document.getElementById('btn-calendar');
+        btnAlert.addEventListener('click', () => {
+            Swal.fire({
+                title: "Calendario  de verificacion",
+                imageUrl: "/img/Verificacion.png",
+                imageWidth: 480,
+                imageHeight: 320,
+                imageAlt: "Custom image"
+            });
+        });
     </script>
 @endsection
