971d6eb0ff33d0f8cb4b03f1909aa680f1440efa
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2024-12-12 16:53:57 -0600

    modificacion de el background de el login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,40 +10,11 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+    {{-- css --}}
+    <link rel="stylesheet" href="">
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <style>
-        /* Evitar que la pantalla se mueva */
-        body, html {
-            overflow: hidden;
-            height: 100%;
-            margin: 0;
-        }
-
-        /* Fondo de la pantalla */
-        .bg-transparent {
-            position: relative;
-            background-image: url('{{ asset('img/fondo.jpg') }}');
-            background-size: cover;
-            background-position: center;
-            background-attachment: fixed;
-            height: 100%;
-        }
-
-        /* Capa negra con opacidad encima de la imagen */
-        .bg-transparent::before {
-            content: '';
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: rgba(0, 0, 0, 0.2); /* Capa negra con opacidad */
-            z-index: 0; 
-        }
-    </style>
 </head>
 <body class="font-sans antialiased text-gray-900">
 
