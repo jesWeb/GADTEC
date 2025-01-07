a6bc1a52157de31a638a9dd2ba63bf4fe41ae4b9
MitziAlRP <mitzi@MacBook-Pro-de-mitzi.local>
2025-01-07 10:33:20 -0600

    Solucion de input en servicios

--- resources/views/modulos/servicios/index.blade.php
+++ resources/views/modulos/servicios/index.blade.php
@@ -179,14 +179,14 @@
                                         </form>
                                             @if($servicio->fecha_servicio === now()->toDateString() || $servicio->prox_servicio === now()->toDateString())
                                                 @if($servicio->estatus == "Mantenimiento")
-                                                    <a href="{{ route('liberar',['id' => $servicio->id_servicio]) }}"
+                                                    <a href="{{ route('liberar',['id' => $servicio->id_servicio]) }}" title="Automovil disponible"
                                                     class="inline-flex items-center justify-center w-8 h-8 text-indigo-800 transition bg-white border border-indigo-600 rounded-md shadow-md hover:bg-white hover:text-white hover:border-blue-950 focus:outline-none focus:ring-2 focus:ring-indigo-500">
                                                     <svg class="w-6 h-6 text-indigo-800 dark:text-dark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.757 6 3.24 10.95a1.05 1.05 0 0 0 0 1.549l5.611 5.088m5.73-3.214v1.615a.948.948 0 0 1-1.524.845l-5.108-4.251a1.1 1.1 0 0 1 0-1.646l5.108-4.251a.95.95 0 0 1 1.524.846v1.7c3.312 0 6 2.979 6 6.654v1.329a.7.7 0 0 1-1.345.353 5.174 5.174 0 0 0-4.652-3.191l-.003-.003Z"/>
                                                     </svg>
                                                     </a>
                                                 @elseif($servicio->estatus == "En servicio")
-                                                    <a href="{{ route('liberar',['id' => $servicio->id_servicio]) }}"
+                                                    <a href="{{ route('liberar',['id' => $servicio->id_servicio]) }}" title="Automovil disponible"
                                                     class="inline-flex items-center justify-center w-8 h-8 text-indigo-800 transition bg-white border border-indigo-600 rounded-md shadow-md hover:bg-white hover:text-white hover:border-blue-950 focus:outline-none focus:ring-2 focus:ring-indigo-500">
                                                     <svg class="w-6 h-6 text-indigo-800 dark:text-dark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.757 6 3.24 10.95a1.05 1.05 0 0 0 0 1.549l5.611 5.088m5.73-3.214v1.615a.948.948 0 0 1-1.524.845l-5.108-4.251a1.1 1.1 0 0 1 0-1.646l5.108-4.251a.95.95 0 0 1 1.524.846v1.7c3.312 0 6 2.979 6 6.654v1.329a.7.7 0 0 1-1.345.353 5.174 5.174 0 0 0-4.652-3.191l-.003-.003Z"/>
