599e3f058cb520d0e2ee0350f3826b94d29ac576
Mitzi Alondra <al222110768@gmail.com>
2025-03-27 17:29:08 -0600

    Se agregan correcciones, vista de perfil y validaciones en solicitudes

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Usuarios; 
+use Illuminate\Support\Facades\Storage;
 
 class PerfilController extends Controller
 {
@@ -11,9 +14,8 @@ class PerfilController extends Controller
      */
     public function index()
     {
-        //
-        return view('perfil.index');
-
+        $usuario = Auth::user(); 
+        return view('perfil.index', compact('usuario'));
     }
 
     /**
@@ -51,10 +53,81 @@ class PerfilController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $usuario = Auth::user();
+
+        if ($usuario->id_usuario != $id) {
+            abort(403, 'No tienes permiso para actualizar este perfil.');
+        }
+
+        $request->validate([
+            'nombre' => 'required|string|max:50',
+            'app' => 'required|string|max:70',
+            'apm' => 'nullable|string|max:70',
+            'fn' => 'required|date',
+            'sex' => 'required|in:Femenino,Masculino',
+            'email' => 'required|email|max:255|unique:usuarios,email,' . $usuario->id_usuario . ',id_usuario',
+            'foto' => 'nullable|image|mimes:jpg,png,jpeg', 
+        ]);
+
+        $input = $request->only([
+            'nombre', 
+            'app', 
+            'apm', 
+            'fn', 
+            'sex', 
+            'email', 
+            'num_licencia'
+        ]);
+
+        // Manejo de la foto
+        if ($request->hasFile('foto')) {
+            $file = $request->file('foto');
+
+            $img2 = date('Ymd_His_') . '_' . $file->getClientOriginalName();
+            $file->move(public_path('img/usuarios'), $img2);
+
+           if ($usuario->foto && $usuario->foto != "shadow.png") {
+                $oldImagePath = public_path('img/usuarios/' . $usuario->foto);
+                if (file_exists($oldImagePath)) {
+                    unlink($oldImagePath);
+                }
+           }
+
+            $input['foto'] = $img2;
+        }
+
+        $usuario->update($input);
+
+        return redirect()->route('perfil.user', $usuario->id_usuario)->with('success', 'Perfil actualizado correctamente.');
     }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function eliminarFoto($id)
+    {
+        $usuario = Auth::user();
+        if ($usuario->id_usuario != $id) {
+            abort(403, 'No tienes permiso para eliminar esta foto.');
+        }
+
+        // Eliminar la imagen actual si no es la predeterminada
+        if ($usuario->foto && $usuario->foto != "shadow.png") {
+            $oldImagePath = public_path('img/usuarios/' . $usuario->foto);
+            if (file_exists($oldImagePath)) {
+                unlink($oldImagePath);
+            }
+        }
+
+        // Asignar imagen predeterminada
+        $usuario->update(['foto' => 'shadow.png']);
+
+        return back()->with('success', 'Foto de perfil eliminada.');
+    }
+
+    
 
     /**
      * Remove the specified resource from storage.
@@ -63,4 +136,6 @@ class PerfilController extends Controller
     {
         //
     }
+    
+    
 }
